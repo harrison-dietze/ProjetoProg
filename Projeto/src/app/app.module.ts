3c7aa5bec6b79eb5a9d398a624110e1f49576f81
Harrison Guinter <harrison.dietze@apollusehs.com.br>
2021-12-06 20:36:16 -0300

    Revert "criando home"
    This reverts commit 64214c30767fc4ce50ec5492f44819580fed05d0.

--- Projeto/src/app/app.module.ts
+++ Projeto/src/app/app.module.ts
@@ -6,13 +6,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { LoginService } from './login.service';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CookieService } from 'ngx-cookie-service';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, HomeComponent],
+  declarations: [AppComponent, LoginComponent],
   imports: [HttpClientModule, BrowserModule, ReactiveFormsModule],
-  providers: [LoginService, CookieService],
+  providers: [LoginService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
