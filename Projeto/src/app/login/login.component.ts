2956b14522c72b7a1e477ba61d6bf0c534bceb02
Harrison Guinter <harrison.dietze@apollusehs.com.br>
2021-12-06 16:40:33 -0300

    corrigindo

--- Projeto/src/app/login/login.component.ts
+++ Projeto/src/app/login/login.component.ts
@@ -10,29 +10,39 @@ import { Usuario } from './usuario.interface';
 })
 export class LoginComponent implements OnInit {
   usuarioLogado: Usuario;
+  senhaValidacao = new FormControl('');
 
   loginForm: FormGroup;
   dados: FormData;
 
-  isCriacao = true;
+  isCriacao: boolean = true;
   resultado: any;
-  title = 'Projeto';
+  isSenhasIguais: boolean = true;
+  marcarNome: boolean = false;
+  marcarSenha: boolean = false;
 
   constructor(private LoginService: LoginService) {}
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      nome: new FormControl('', {
-        validators: [Validators.required],
-      }),
-      senha: new FormControl('', { validators: [Validators.required] }),
+      nome: new FormControl(''),
+      senha: new FormControl(''),
     });
+
+    this.senhaValidacao = new FormControl('');
   }
 
   onLogin(isCriacao: boolean) {
+    if (this.verificarCamposObrigatorios()) {
+      return;
+    }
+
     if (!isCriacao) {
       this.onEntrarUsuario();
     } else {
+      if (!this.verificarSenhasIguais()) {
+        return;
+      }
       this.onCriarUsuario();
     }
   }
@@ -66,5 +76,31 @@ export class LoginComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  verificarSenhasIguais() {
+    if (this.senhaValidacao.value != this.loginForm.value.senha) {
+      this.isSenhasIguais = false;
+      return false;
+    } else {
+      this.isSenhasIguais = true;
+      return true;
+    }
+  }
+
+  verificarCamposObrigatorios() {
+    if (this.loginForm.value.nome.toString().length < 4) {
+      this.marcarNome = true;
+    } else {
+      this.marcarNome = false;
+    }
+    if (this.loginForm.value.senha.toString().length < 6) {
+      this.marcarSenha = true;
+    } else {
+      this.marcarSenha = false;
+    }
+    if (this.marcarSenha == true || this.marcarNome == true) {
+      return false;
+    }
   }
 }
